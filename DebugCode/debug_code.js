1818c16d042ffda9a9b71a70c729f074d27f0d8f
sumayyah <sumayyahismail18@gmail.com>
2025-03-10 17:21:56 -0400

    message

--- DebugCode/debug_code.js
+++ DebugCode/debug_code.js
@@ -3,7 +3,13 @@ function performOperation(){
     let num2 = parseInt(document.getElementById('input2').value);
     if (!isNaN(num1) && !isNaN(num2)){
         let result = multiply(num1, num2);
+        let result2 = add(num1, num2);
+        let result3 = sub(num1, num2);
+        let result4 = div(num1,num1);
         displayResult(result);
+        displayResult(result2);
+        displayResult(result3);
+        displayResult(result4);
     }else{
         displayResult('Please enter valid numbers');
     }
@@ -13,8 +19,20 @@ function multiply(a,b){
     debugger;
     return a*b;
 }
+function sub(a,b){
+    debugger;
+    return a-b;
+}
+function add(a,b){
+    debugger;
+    return a+b;
+}
+function div(a,b){
+    debugger;
+    return a/b;
+}
 function displayResult(result){
     
     const resultElement = document.getElementById('result');
-    resultElement.textContent = `The result is ${result}`;
+    resultElement.textContent = `The result of is ${result}`;
 }
