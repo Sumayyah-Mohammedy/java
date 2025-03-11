22737c3de43b846cbb99d36f199134bde432f9ae
sumayyah <sumayyahismail18@gmail.com>
2025-03-11 16:48:56 -0400

    message

---
+++ todolist/todo_list.js
@@ -0,0 +1,34 @@
+const taskInput = document.getElementById('taskInput');
+const addTaskBtn = document.getElementById('addTaskBtn');
+const clearCompletedBTn = document.getElementById('clearCompletedBTn');
+const taskList = document.getElementById('taskList');
+ let tasks = [];
+ function addTask(){
+    const taskTest = taskInput.value.trim();
+    if(taskTest !== ''){
+        tasks.push({task: taskTest});
+        taskInput.value='';
+        displayTask();
+    }
+ }
+ function displayTask(){
+    taskList.innerHTML = '';
+    tasks.forEach((task, index) => {
+        const li = document.createElement('li');
+        li.innerHTML = `<input type="checkbox" id="task-${index}"${task.completed? "checked":""}>
+        <label for="task-${index}">${task.test}</label>`;
+        li.querySelector("input").addEventListener("change", () => toggleTask(index));
+        taskList.appendChild(li);
+    });
+ }
+ function toggleTask(index){
+    tasks[index].completed = !tasks[index].completed;
+    displayTask();
+ }
+ function clearCompletedTask(){
+    tasks = tasks.filter(task => !task.completed);
+    displayTask();
+ }
+ addTaskBtn.addEventListener("click", addTask);
+ clearCompletedBTn.addEventListener("click", clearCompletedTask);
+ displayTask();
