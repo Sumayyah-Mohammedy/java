3f2f85860e033d2d662e971f39bf8ef7bce24d3b
sumayyah <sumayyahismail18@gmail.com>
2025-03-11 17:17:24 -0400

    message

--- todolist/todo_list.js
+++ todolist/todo_list.js
@@ -11,22 +11,27 @@ const taskList = document.getElementById('taskList');
         displayTask();
     }
  }
- function displayTask(){
-    taskList.innerHTML = '';
+ function displayTask() {
+    taskList.innerHTML = "";
     tasks.forEach((task, index) => {
-        const li = document.createElement('li');
-        li.innerHTML = `<input type="checkbox" id="task-${index}"${task.completed? "checked":""}>
-        <label for="task-${index}">${task.test}</label>`;
+        const li = document.createElement("li");
+        li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}>
+            <label for="task-${index}">${task.test}</label>`;
         li.querySelector("input").addEventListener("change", () => toggleTask(index));
         taskList.appendChild(li);
     });
- }
+}
  function toggleTask(index){
     tasks[index].completed = !tasks[index].completed;
     displayTask();
  }
  function clearCompletedTask(){
     tasks = tasks.filter(task => !task.completed);
+    displayTask();
+ }
+
+ function clearallTasks(){
+    tasks = [];
     displayTask();
  }
  addTaskBtn.addEventListener("click", addTask);
